7475939af94a4fcf1046522f736155f752f85074
1Czerwony <ra120114@uem.br>
2023-04-15 18:14:16 -0300

    n aguento mais

--- mainpt3.py
+++ mainpt3.py
@@ -3,7 +3,7 @@
 # Vitor Felipe de Souza Siqueira | RA:122907
 # Professor: Marco Aurélio Lopes Barbosa
 #------------------------------------------------------------------------------------------------------
-import math
+import math, random, time
 from collections import deque
 #------------------------------------------------------------------------------------------------------
 
@@ -16,8 +16,6 @@ class Grafo:     # Classe que representa um Grafo
         if u in range(len(self.vertices)) and v in range(len(self.vertices)) and (u,v) not in self.c :
             self.vertices[u][0].append(v)
             self.c[(u,v)] = c
-        else:
-            print(f'A aresta ({u},{v}) é inválida')
             
 #------------------------------------------------------------------------------------------------------
 
@@ -44,19 +42,25 @@ def BFS(g, s):
     
 #------------------------------------------------------------------------------------------------------
 
-# Retorna True se um grafo é uma rede (não possui arestas antiparalelas) e False, caso contrário
-def verifica_rede(r):
-    for aresta in r.c:
-        if (aresta[1], aresta[0]) in r.c:
+# Retorna True se um grafo é uma rede e False, caso contrário
+def verifica_rede(r,s,t): # Argumentos: r é uma rede, s é o vértice de origem e t é o vértice de destino
+    if s not in range(len(r.vertices)) or t not in range(len(r.vertices)):      # Se s ou t não forem vértices do grafo, retorna False
+        return False
+    for aresta in r.c:                       
+        if aresta[1] == s or aresta[0] == t:                                    # Se existir aresta (v,s) ou (t,u), retorna False                                          
+            return False                                                        
+        if (aresta[1], aresta[0]) in r.c:                                       # Se existir arestas antiparalelas, retorna False
+            return False
+    for v in range(len(r.vertices)):                                            # Para todo vértice v deve existir um caminho de s para t que passe por v.
+        if encontra_caminho(r,s,v) == None or encontra_caminho(r,v,t) == None:
             return False
     return True
         
 #------------------------------------------------------------------------------------------------------
 
 # Retorna True se um fluxo f é válido na rede r ou False, caso contrário
-def verifica_fluxo(r, f):
-    if verifica_rede(r) == False:
-        return False
+# Este algoritmo assume que a rede r é válida
+def verifica_fluxo(r,f): # Argumentos: r é uma rede, f é um fluxo
     for aresta in r.c:
         if f[aresta] > r.c[aresta] or f[aresta] < 0:            # Se o fluxo de uma aresta for maior que sua capacidade ou menor que 0, retorna False
             return False
@@ -64,8 +68,9 @@ def verifica_fluxo(r, f):
 
 #------------------------------------------------------------------------------------------------------
 
-# Gera uma rede residual a partir de uma rede original r e um fluxo f
-def gera_rede_residual(r, f):
+# Gera uma rede residual a partir de uma rede original r e um fluxo 
+# Este algoritmo assume que a rede r é válida
+def gera_rede_residual(r,f): # Argumentos: r é uma rede, f é um fluxo
     if verifica_fluxo(r,f) == False:                             # Se o fluxo for inválido, retorna None
         return None
     Gf = Grafo(len(r.vertices))                                              
@@ -82,8 +87,7 @@ def gera_rede_residual(r, f):
 #------------------------------------------------------------------------------------------------------
 
 # Encontra e retorna as arestas de um caminho entre s e t em uma rede r
-# s e t devem ser os vértices de origem e destino, respectivamente, caso contrário, o algoritmo não funcionará corretamente
-def encontra_caminho(r, s, t):
+def encontra_caminho(r, s, t): # Argumentos: r é uma rede, s e t são vértices da rede r
     if r.c == {} or r.vertices == []:       # Retorna None se a rede for vazia
         return None
     BFS(r, s)
@@ -102,16 +106,14 @@ def encontra_caminho(r, s, t):
 #------------------------------------------------------------------------------------------------------
 
 # Algoritmo de Edmonds-Karp para encontrar o fluxo máximo em uma rede
-# s e t devem ser os vértices de origem e destino, respectivamente, caso contrário, o algoritmo não funcionará corretamente
-def Edmonds_Karp(g,s,t):
-    if verifica_rede(g) == False:                       # Se a rede não for válida, retorna None
-        return None
+# Este algoritmo assume que a rede g é válida
+def Edmonds_Karp(g,s,t): # Argumentos: g é uma rede, s é o vértice de origem e t é o vértice de destino
     f={}
     for aresta in g.c:
         f[aresta] = 0
     while True:
         Gf = gera_rede_residual(g,f)    
-        p = encontra_caminho(Gf,s,t)                    # p é um caminho encontrado entre s e t na rede residual
+        p = encontra_caminho(Gf,s,t)                    # p é um caminho encontrado usando BFS entre s e t na rede residual
         if p == None:                                   # Se não existir caminho entre s e t, retorna o fluxo f
             return f
         cf = min(p.values())                            # cf é o valor de fluxo máximo que pode ser adicionado ao fluxo f
@@ -121,14 +123,38 @@ def Edmonds_Karp(g,s,t):
             else:                                       # Se a aresta (u,v) de p não pertence à rede original, decrementa o fluxo f(v,u) em cf
                 f[(aresta[1],aresta[0])] -= cf
             
+#------------------------------------------------------------------------------------------------------
+            
+# Gera uma rede aleatória válida com n vértices
+# A rede gerada possui s = 0 e t = n-1
+# A probabilidade de uma aresta ser adicionada é 0.5
+# O valor de cada aresta é um inteiro aleatório entre 1 e 50
+def gera_rede_aleatória(n): # Argumentos: n é o número de vértices da rede
+    if n < 2:                                               # Se n < 2, não é possível gerar uma rede válida
+        return None
+    p = 0.5
+    g = Grafo(n)
+    while verifica_rede(g,0,n-1) == False:                  # Sorteia novas arestas até que a rede seja válida
+        for i in range(n):
+            for j in range(i+1,n):                          
+                if random.random() <= p:                    # Para cada par de vértices, sorteia uma aresta com probabilidade p
+                    g.add_aresta(i,j,random.randint(1,50))
+    return g,0,n-1
+            
 #TESTES------------------------------------------------------------------------------------------------------
 
 # Rede inválida (arestas antiparalelas)
 g = Grafo(2)
 g.add_aresta(0,1,1)
 g.add_aresta(1,0,2)
+assert verifica_rede(g,0,1) == False
 
-assert verifica_rede(g) == False
+# Rede inválida (não existe caminho entre s e t que passe pelo vértice 2)
+k = Grafo(4)
+k.add_aresta(0,1,1)
+k.add_aresta(1,3,1)
+k.add_aresta(2,3,1)
+assert verifica_rede(k,0,3) == False
 
 # Rede da figura 26.1 do livro
 s,v1,v2,v3,v4,t = 0,1,2,3,4,5
@@ -142,8 +168,7 @@ r.add_aresta(v3,v2,9)
 r.add_aresta(v3,t,20)
 r.add_aresta(v4,v3,7)
 r.add_aresta(v4,t,4)
-
-assert verifica_rede(r) == True
+assert verifica_rede(r,s,t) == True
 
 # Fluxo da figura 26.1 do livro (válido na rede r)
 f = {}
@@ -156,20 +181,18 @@ f[(v3,v2)] = 4
 f[(v3,t)] = 15
 f[(v4,v3)] = 7
 f[(v4,t)] = 4
+assert verifica_fluxo(r,f) == True
 
 # Fluxo inválido na rede r
 f2 = {}
 f2[(s,v1)] = 17
-
-# Teestes de fluxo na rede r
-assert verifica_fluxo(r,f) == True
 assert verifica_fluxo(r,f2) == False
 
 # Rede residual inválida
-assert gera_rede_residual(r, f2) == None
+assert gera_rede_residual(r,f2) == None
 
 # Rede residual da figura 26.4(b) do livro
-rf = gera_rede_residual(r, f)
+rf = gera_rede_residual(r,f)
 assert rf != None
 assert rf.c[(s,v1)] == 5
 assert rf.c[(v1,s)] == 11
@@ -222,7 +245,7 @@ h.add_aresta(c,e,4)
 h.add_aresta(d,t1,9)
 h.add_aresta(e,d,1)
 h.add_aresta(e,t1,6)
-assert verifica_rede(h) == True
+assert verifica_rede(h,s1,t1) == True
 
 # Verifica fluxo máximo da rede h gerado pelo algoritmo Edmonds_Karp
 f4 = Edmonds_Karp(h,s1,t1)
@@ -239,3 +262,29 @@ assert f4[(c,e)] == 3
 assert f4[(d,t1)] == 8
 assert f4[(e,d)] == 1
 assert f4[(e,t1)] == 6
+
+# -----------------------------------------------------------------------------------------------
+
+# Gera rede aleatória inválida (menos de 2 vértices)
+assert gera_rede_aleatória(1) == None
+assert gera_rede_aleatória(0) == None
+assert gera_rede_aleatória(-1) == None
+ 
+# Gera 100 redes aleatórias com 100 vértices e verifica se são válidas
+inicio = time.time()
+for i in range(100):
+    #n = random.randint(2,200)
+    rede,s2,t2 = gera_rede_aleatória(100)
+    assert verifica_rede(rede,0,99) == True
+final = time.time()
+print('Tempo de execução: %2fs' % (final - inicio))
+
+# Gera 100 redes aleatórias com 100 vértices e verifica se o fluxo máximo é válido
+inicio = time.time()
+for i in range(100):
+    #n = random.randint(2,200)
+    rede,s2,t2 = gera_rede_aleatória(100)
+    fluxo = Edmonds_Karp(rede,s2,t2)
+    assert verifica_fluxo(rede,fluxo) == True
+final = time.time()
+print('Tempo de execução: %2fs' % (final - inicio))
